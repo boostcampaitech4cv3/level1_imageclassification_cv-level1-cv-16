46546030480ef2c34757073d1fc6a4c895afbfde
juye-ops <kjye.ops@gmail.com>
2022-11-22 20:45:50 +0900

    Remove Code file

--- code/dataloader/dataloader.py
+++
@@ -1,5 +0,0 @@
-from torch.utils.data import Dataset, DataLoader
-
-class CustomDataLoader(DataLoader):
-    def __init__(self, dataset, batch_size, shuffle, num_workers):
-        super(CustomDataLoader, self).__init__(dataset, batch_size = batch_size, shuffle=shuffle, num_workers=num_workers)
